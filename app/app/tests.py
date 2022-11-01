bd48827b50aef5bcc740c3c59c096e2e25d4cd8d
Maria Negri <maria.negri@oliphantusa.com>
2022-11-01 09:00:04 -0700

    Configure database in django

---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+'''
+Sample Tests
+
+'''
+
+
+from django.test import SimpleTestCase
+from . import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(8,3)
+        print('res', res)
+        self.assertEqual(res, 11)
+        
+    def test_substract_numbers(self):
+        res = calc.substract(8,3)
+        print('res', res)
+        self.assertEqual(res, 5)
+        
+        
